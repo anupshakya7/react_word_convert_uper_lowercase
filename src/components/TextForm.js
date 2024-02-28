fd9a7840b433035e46378e7166f1ef33be340d1a
unknown <anupshk39@gmail.com>
2024-02-28 11:40:09 +0545

    New About Component Added

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -11,6 +11,37 @@ export default function TextForm(props) {
     setText(newtext);
   };
 
+  const handleReversetextClick = ()=>{
+    let newtext = reverseString(text);
+    setText(newtext);
+  }
+
+  const handleCopytextClick = () =>{
+    console.log("I am Copy");
+    let text = document.getElementById('myBox');
+    text.select();
+    text.setSelectionRange(0,9999);
+    navigator.clipboard.writeText(text.value);
+  }
+
+  const handleExtraSpacetextClick = () =>{
+    let newtext = text.split(/[ ]+/);
+    setText(newtext.join(" "));
+  }
+
+  const handleCleartextClick= () =>{
+    let newtext = '';
+    setText(newtext);
+  }
+
+  const reverseString = (str)=>{
+    let newString = "";
+    for(let i = str.length -1;i>=0;i--){
+      newString += str[i];
+    }
+    return newString;
+  }
+
   const handleOnChange = (event) => {
     setText(event.target.value);
   };
@@ -36,6 +67,19 @@ export default function TextForm(props) {
         <button className="btn btn-primary m-2" onClick={handleLoClick}>
           Convert to Lowercase
         </button>
+        <button className="btn btn-primary m-2" onClick={handleReversetextClick}>
+          Reverse Text
+        </button>
+        <button className="btn btn-primary m-2" onClick={handleCopytextClick}>
+          Copy Text
+        </button>
+        <button className="btn btn-primary m-2" onClick={handleExtraSpacetextClick}>
+          Remove Extra Space
+        </button>
+        <button className="btn btn-primary m-2" onClick={handleCleartextClick}>
+          Clear Text
+        </button>
+        
       </div>
       <div className="container my-4">
         <h2>Your Text Summary</h2>
